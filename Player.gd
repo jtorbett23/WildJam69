8ae4a08c9aedfed302c1cc4c831995bce6348b5d
jtorbett23 <56300912+jtorbett23@users.noreply.github.com>
2024-05-14 21:26:21 +0100

    add camera controls

--- Player.gd
+++ Player.gd
@@ -3,16 +3,42 @@ extends CharacterBody3D
 @export var speed = 4.0
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+var mouse_sensitivity : float = 0.001
+var twist_input : float = 0.0
+var pitch_input : float = 0.0
 
-func _physics_process(delta):
-	velocity.y -= gravity * delta
+@onready var twist_pivot : Node3D = $TwistPivot
+@onready var pitch_pivot : Node3D = $TwistPivot/PitchPivot
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _physics_process(delta) -> void:
+	if not is_on_floor():
+		velocity.y -= gravity * delta
 	get_input()
 	move_and_slide()
 
+	if Input.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	twist_pivot.rotate_y(twist_input)
+	pitch_pivot.rotate_x(pitch_input)
+	pitch_pivot.rotation.x = clamp(pitch_pivot.rotation.x, deg_to_rad(-30), deg_to_rad(30))
+	twist_input = 0.0
+	pitch_input = 0.0
 
-func get_input():
+func get_input() -> void:
 	var vy = velocity.y
 	velocity = Vector3.ZERO
-	velocity.x += Input.get_axis("ui_left", "ui_right") * speed
-	velocity.z += Input.get_axis("ui_up", "ui_down") * speed
+	var input_dir : Vector2 = Input.get_vector("left","right","forward", "back")
+	var direction : Vector3 = (twist_pivot.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	velocity.y = vy
+func _unhandled_input(event : InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			twist_input = - event.relative.x * mouse_sensitivity
+			pitch_input = - event.relative.y * mouse_sensitivity
